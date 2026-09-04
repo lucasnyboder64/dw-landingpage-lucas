585e6bc5df62a3bc09f70bc13206bde35f970ce1
Lucas <lucasnyboder64@gmail.com>
2026-09-04 10:52:18 +0200

    Tilføjet facilities sites advantages

--- js/index.js
+++ js/index.js
@@ -4,6 +4,8 @@ const headerElement = document.querySelector(".header");
 const wrapperElement = document.querySelector(".wrapper");
 const servicesElement = document.querySelector(".services");
 const facilitiesElement = document.querySelector(".facilities");
+const sitesElement = document.querySelector(".sites");
+const advantagesElement = document.querySelector(".advantages");
 //eksempel på at udskrive alle overskrifter i services i konsollen:
 services.forEach(service => console.log(service.headline));
 
@@ -37,10 +39,62 @@ function setServices() {
     });
 }
 
-function setFacilities(){
-    
+function setFacilities() {
+    let headline = document.createElement("h1")
+    headline.textContent = facilities.headline;
+    facilitiesElement.append(headline);
+    facilities.options.forEach((option) => {
+        let icon = document.createElement("img");
+        let headline = document.createElement("h2");
+        let text = document.createElement("p");
+
+        icon.setAttribute("src", option.icon);
+        headline.textContent = option.headline;
+        text.textContent = option.text;
+
+        facilitiesElement.append(icon, headline, text);
+    });
 }
 
+function setSites() {
+    let headline = document.createElement("h1");
+    let text = document.createElement("p");
+    let icon = document.createElement("img");
+
+    headline.textContent = sites.headline;
+    text.textContent = sites.text;
+    icon.setAttribute("src", sites.btnicon);
+
+    sitesElement.append(headline, text, icon);
+
+    sites.places.forEach((place) => {
+        const image = document.createElement("img");
+        let name = document.createElement("h2");
+        let city = document.createElement("p");
+
+        image.setAttribute("src", place.img);
+        name.textContent = place.name;
+        city.textContent = place.city;
+        sitesElement.append(image,name,city);
+    });
+}
+
+function setAdvantages(){
+    advantages.forEach((advantage) => {
+        let icon = document.createElement("img");
+        let headline = document.createElement("h1");
+        let text = document.createElement("p");
+
+        icon.setAttribute = advantage.icon;
+        headline.textContent = advantage.headline;
+        text.textContent = advantage.text;
+
+        advantagesElement.append(icon, headline, text);
+    });
+}
 
 setServices();
 setHero();
+setFacilities();
+setSites();
+setAdvantages();
